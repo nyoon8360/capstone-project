44e3854aaaeba225c6179df587826d0c327196d0
O-Farnum <ofarnum3@gmail.com>
2024-09-23 13:41:53 -0500

    Pokemon Mapper

--- server/src/main/java/org/lonic/data/PokemonInstanceJDBCRepository.java
+++ server/src/main/java/org/lonic/data/PokemonInstanceJDBCRepository.java
@@ -1,4 +1,40 @@
 package org.lonic.data;
 
-public class PokemonInstanceJDBCRepository {
+import org.lonic.data.mappers.PokemonInstanceMapper;
+import org.lonic.models.PokemonInstance;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class PokemonInstanceJDBCRepository implements PokemonInstanceRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public PokemonInstanceJDBCRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<PokemonInstance> findAll() {
+        final String sql = "select pokemon_instance_id, pokemon_name, app_user_id, max_hp, attack, defense, special_attack, special_defense, speed "
+                + "from pokemon_instance;";
+        return jdbcTemplate.query(sql, new PokemonInstanceMapper());
+    }
+
+    public static List<PokemonInstance> getByUserId(int userId) {
+        return null;
+    }
+
+    public static PokemonInstance add(PokemonInstance pokemonInstance) {
+        return null;
+    }
+
+    public static boolean deleteById(int id) {
+        return false;
+    }
+
+    public static boolean update(PokemonInstance pokemonInstance) {
+        return false;
+    }
+
 }
+
---
+++ server/src/test/java/org/lonic/data/PokemonInstanceJDBCRepositoryTest.java
@@ -0,0 +1,13 @@
+package org.lonic.data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PokemonInstanceJDBCRepositoryTest {
+
+    @Test
+    void findAll() {
+        
+    }
+}
