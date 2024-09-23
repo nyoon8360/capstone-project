b5df426a0ca7efe4251d24cd83eef85f9d5ce6b1
Nicholas Yoon <nyoon8360@gmail.com>
2024-09-23 11:37:33 -0400

    cleaned and refactored classes

--- client/pokemon-safari/src/App.js
+++ client/pokemon-safari/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 
 function App() {
   return (<Router>
